19063f9091727872e4369f022d3f072927f3b36f
jiho <jihopark09@naver.com>
2024-05-04 17:45:48 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link, BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import PokemonList from './components/PokemonList';
-import {  } from 'react-router-dom';
+import {} from 'react-router-dom';
 import PokemonDetail from './components/PokemonDetail';
 
 function App() {
@@ -10,15 +10,14 @@ function App() {
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="App">
         <nav>
-            <h1>
-                <Link to="./">Pokemon List</Link>
-            </h1>
+          <h1>
+            <Link to="./">Pokemon List</Link>
+          </h1>
         </nav>
         <main>
           <Routes>
-            <Route path='/' element={<PokemonList/>}/>
-            <Route path='/pokemon/:id' element={<PokemonDetail/>}/>
-
+            <Route path="/" element={<PokemonList />} />
+            <Route path="/pokemon/:id" element={<PokemonDetail />} />
           </Routes>
         </main>
       </div>
